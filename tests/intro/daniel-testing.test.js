52cb3dbe561626666d12e014402674aa76313de5
Daniel Hermon <danielhermonsync@gmail.com>
2020-01-03 11:22:19 +0200

    added the presentation in pdf and pptx formats

--- tests/intro/daniel-testing.test.js
+++ tests/intro/daniel-testing.test.js
@@ -1,12 +1,9 @@
-const { should } = require('chai');
 describe('Daniel is', () => {
    it('cool', function() {
    });
    it('smart', function() {
    });
    it('modest', function(){
-   });
-   it('in the best team in the panda (GO TESLA!)', function(){
    });
 });
 describe('Daniel should', function() {
